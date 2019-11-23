56fa6709e951944ea2df2940d0616fa4d1bcdd89
sarvang008 <sarvang.m@somaiya.edu>
2019-11-23 16:35:19 +0530

    Used async await instead of promises

--- Config/db.js
+++ Config/db.js
@@ -2,20 +2,36 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDb = () => {
-  mongoose
-    .connect(db, {
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: true,
       useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log('Mongo Db connected');
-    })
-    .catch(err => {
-      console.error(err.message);
-      process.exit(1);
     });
+
+    console.log('Mongo db connected');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 };
+
+// const connectDb = () => {
+//  mongoose
+//    .connect(db, {
+//      useNewUrlParser: true,
+//      useCreateIndex: true,
+//      useFindAndModify: true,
+//      useUnifiedTopology: true
+//    })
+//    .then(() => {
+//      console.log('Mongo Db connected');
+//    })
+//    .catch(err => {
+//      console.error(err.message);
+//      process.exit(1);
+//   });
+// };
 module.exports = connectDb;
